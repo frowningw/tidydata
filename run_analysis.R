49b7e2098e02be0bceadef3cb99c9d51b966d7a2
frowningw <b04303073@g.ntu.edu.tw>
2021-01-30 00:54:40 +0800

    iniitate

---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+setwd(dir="/users/will/Desktop")
+sjtest <- read.table("subject_test.txt")
+xtest <- read.table("X_test.txt")
+ytest <- read.table("y_test.txt")
+sjtrain <- read.table("subject_train.txt")
+xtrain <- read.table("X_train.txt")
+ytrain <- read.table("y_train.txt")
+train <- cbind(sjtrain, ytrain, xtrain)
+test <- cbind(sjtest, ytest, xtest)
+var <- read.table("features.txt")[,2]
+df_merge <- rbind(test, train)
+colnames(df_merge)[1:2] <- c("subject", "activity")
+colnames(df_merge)[3:563] <- var
+varind <- c(1,1,grepl("mean", var) + grepl("std", var))
+df_merge <- df_merge[,grep(1,varind)]
+df_merge[,2] <- gsub(1, "Walking", df_merge[,2])
+df_merge[,2] <- gsub(2, "Walking Upstairs",df_merge[,2]) 
+df_merge[,2] <- gsub(3, "Walking Downstairs",df_merge[,2])
+df_merge[,2] <- gsub(4, "Sitting",df_merge[,2])
+df_merge[,2] <- gsub(5, "Standing", df_merge[,2])
+df_merge[,2] <- gsub(6, "Laying", df_merge[,2])
